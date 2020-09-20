2893a1326ec84d543b1d38b46a82e2f59d563a89
Vasyl Biliba <58170018+spiritufuerte@users.noreply.github.com>
2020-09-20 15:22:11 +0300

    Updates

---
+++ precache-manifest.b568d570bd0fea8f3c048f5c88f84a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df894e1552ae43497899a467f3f34b4a",
+    "url": "/spiritufuerte/react-traslater/index.html"
+  },
+  {
+    "revision": "788cf6c59b1045609216",
+    "url": "/spiritufuerte/react-traslater/static/css/main.d9858454.chunk.css"
+  },
+  {
+    "revision": "39e6e040d40c2a6cbc04",
+    "url": "/spiritufuerte/react-traslater/static/js/2.c8f9f5b8.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/spiritufuerte/react-traslater/static/js/2.c8f9f5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788cf6c59b1045609216",
+    "url": "/spiritufuerte/react-traslater/static/js/main.79797695.chunk.js"
+  },
+  {
+    "revision": "4c0eb4e2d79b2936fc9d",
+    "url": "/spiritufuerte/react-traslater/static/js/runtime-main.272c0b29.js"
+  }
+]);
